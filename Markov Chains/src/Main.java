81d758d5a9d32dcf5466077454c226de483be6f0
John Karmozyn <hughkarmozyn@icloud.com>
2024-02-23 10:24:30 -0700

    initial commit

---
+++ Markov Chains/src/Main.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("File Name:"+ s.nextLine());
+
+    }
+}
