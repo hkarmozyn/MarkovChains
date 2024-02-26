0a61b0b0a02e6932d1f5ecc42d700bc9425c9d00
John Karmozyn <hughkarmozyn@icloud.com>
2024-02-26 14:05:08 -0700

    initial driver

--- Markov Chains/src/Main.java
+++ Markov Chains/src/Main.java
@@ -1,13 +1,36 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Dictionary;
 import java.util.Scanner;
 
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner s = new Scanner(System.in);
-        System.out.println("File Name:");
+        Scanner d = new Scanner(System.in);
+        System.out.println("Input File Name: ");
         String fileName = s.nextLine();
+
+
+        MarkovChain mc = new MarkovChain();
+        mc.train(fileName);
+        System.out.println("How many words would you like to generate?");
+        int numWords = d.nextInt();
+
+        System.out.println("Output File Name:");
+        String outFile = s.nextLine();
+
+        String generatedText = mc.generateText(numWords);
+
+        try (PrintWriter pw= new PrintWriter(outFile)) {
+            pw.println(generatedText);
+        }
+
+
+
+
 
     }
 }
